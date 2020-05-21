8cdea70522a238ac41ff5780c45c16c34f3b7ac2
sobriodev <sobriodev@gmail.com>
2020-05-21 20:12:47 +0200

    Refactoring. Created two tests that join previous tests

--- tests/Workspace/WorkspaceTests.cpp
+++ tests/Workspace/WorkspaceTests.cpp
@@ -4,23 +4,15 @@
 
 namespace leddotmatrix {
 
-TEST(WorkspaceTestSuite, makeRectangle_WidthLessThanOne_WidthEqualsToOne) {
-    const auto workspace = Workspace::makeRectangle(0, 1);
+TEST(WorkspaceTestSuite, makeRectangle_SizesEqualToZero_Rectangle1x1Received) {
+    const auto workspace = Workspace::makeRectangle(0, 0);
     ASSERT_EQ(1, workspace.getWidth());
-}
-
-TEST(WorkspaceTestSuite, makeRectangle_HeightLessThanOne_HeightEqualsToOne) {
-    const auto workspace = Workspace::makeRectangle(1, 0);
     ASSERT_EQ(1, workspace.getHeight());
 }
 
-TEST(WorkspaceTestSuite, makeRectangle_WidthTooBigGiven_WidthLimitedToMaxWidth) {
-    const auto workspace = Workspace::makeRectangle(129, 1);
+TEST(WorkspaceTestSuite, makeRectangle_SizesTooBig_Rectangle128x128Received) {
+    const auto workspace = Workspace::makeRectangle(129, 129);
     ASSERT_EQ(128, workspace.getWidth());
-}
-
-TEST(WorkspaceTestSuite, makeRectangle_HeightTooBigGiven_HeightLimitedToMaxSize) {
-    const auto workspace = Workspace::makeRectangle(1, 129);
     ASSERT_EQ(128, workspace.getHeight());
 }
 
