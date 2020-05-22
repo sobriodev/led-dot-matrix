d04ef23b0d53e2b59105c8e8df2a8cb288ec355d
sobriodev <sobriodev@gmail.com>
2020-05-22 18:45:29 +0200

    Tests refactoring

--- tests/Workspace/WorkspaceTests.cpp
+++ tests/Workspace/WorkspaceTests.cpp
@@ -4,6 +4,15 @@
 
 namespace leddotmatrix {
 
+class WorkspaceTestFixture : public ::testing::TestWithParam<int> {};
+INSTANTIATE_TEST_SUITE_P(InvalidSizes, WorkspaceTestFixture, ::testing::Values(-100, -57, -1, 0, 129, 130, 150, 211));
+
+TEST_P(WorkspaceTestFixture, isSizeValid_InvalidSize_FalseReturned) {
+    const int invalidSize = GetParam();
+    const auto actual = Workspace::isSizeValid(invalidSize);
+    ASSERT_FALSE(actual);
+}
+
 TEST(WorkspaceTestSuite, makeRectangle_SizesEqualToZero_Rectangle1x1Received) {
     const auto workspace = Workspace::makeRectangle(0, 0);
     ASSERT_EQ(1, workspace.getWidth());
@@ -20,21 +29,6 @@ TEST(WorkspaceTestSuite, makeRectangle_ValidRectangle_TheSameRectangleReceived) 
     const auto workspace = Workspace::makeRectangle(1, 127);
     ASSERT_EQ(1, workspace.getWidth());
     ASSERT_EQ(127, workspace.getHeight());
-}
-
-TEST(WorkspaceTestSuite, isSizeValid_SizeEqualsToZero_FalseReturned) {
-    const auto result = Workspace::isSizeValid(0);
-    ASSERT_FALSE(result);
-}
-
-TEST(WorkspaceTestSuite, isSizeValid_SizeLessThanZero_FalseReturned) {
-    const auto result = Workspace::isSizeValid(-100);
-    ASSERT_FALSE(result);
-}
-
-TEST(WorkspaceTestSuite, isSizeValid_SizeGreaterThanMaxSize_FalseReturned) {
-    const auto result = Workspace::isSizeValid(129);
-    ASSERT_FALSE(result);
 }
 
 TEST(WorkspaceTestSuite, isSizeValid_ValidSize_TrueReturned) {
