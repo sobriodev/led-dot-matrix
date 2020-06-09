72cd407d0f1bb3cfe6bce6540ada88926b8350c6
sobriodev <sobriodev@gmail.com>
2020-06-09 12:01:30 +0200

    Created WorkspaceInterface abstract class. Workspace class inherits from it.

--- inc/Workspace.h
+++ inc/Workspace.h
@@ -1,18 +1,19 @@
 #ifndef LEDDOTMATRIX_WORKSPACE_H
 #define LEDDOTMATRIX_WORKSPACE_H
 
+#include "WorkspaceInterface.h"
+
 namespace leddotmatrix {
 
-class Workspace {
+class Workspace : public WorkspaceInterface {
 public:
     static constexpr int MIN_SIZE = 1;
     static constexpr int MAX_SIZE = 128;
 
     static Workspace makeRectangle(int width, int height);
-    [[nodiscard]] virtual bool isSizeValid(int size) const;
-    [[nodiscard]] virtual int getWidth() const;
-    [[nodiscard]] virtual int getHeight() const;
-    virtual ~Workspace() noexcept = default;
+    bool isSizeValid(int size) const override;
+    int getWidth() const override;
+    int getHeight() const override;
 private:
     static int getBoundedSize(int size);
     Workspace(int width, int height) noexcept;
---
+++ inc/WorkspaceInterface.h
@@ -0,0 +1,16 @@
+#ifndef LEDDOTMATRIX_WORKSPACEINTERFACE_H
+#define LEDDOTMATRIX_WORKSPACEINTERFACE_H
+
+namespace leddotmatrix {
+
+class WorkspaceInterface {
+public:
+    [[nodiscard]] virtual bool isSizeValid(int size) const = 0;
+    [[nodiscard]] virtual int getWidth() const = 0;
+    [[nodiscard]] virtual int getHeight() const = 0;
+    virtual ~WorkspaceInterface() noexcept = default;
+};
+
+}
+
+#endif //LEDDOTMATRIX_WORKSPACEINTERFACE_H
