9381494f3bd294bce4cc4283ef1caae0daae2004
sobriodev <sobriodev@gmail.com>
2020-06-22 12:21:09 +0200

    All tests now use getFrames method instead of getSerialData

--- tests/FrameBuilder/FrameBuilderTests.cpp
+++ tests/FrameBuilder/FrameBuilderTests.cpp
@@ -13,26 +13,26 @@ using ::testing::Return;
 using ::testing::Each;
 using ::testing::AllOf;
 
-class GetSerialDataTestFixture : public ::testing::TestWithParam<int> {};
-INSTANTIATE_TEST_SUITE_P(VariousNumberOfDevicesUsed, GetSerialDataTestFixture, ::testing::Values(1, 8, 16129));
+class GetFramesTestFixture : public ::testing::TestWithParam<int> {};
+INSTANTIATE_TEST_SUITE_P(VariousNumberOfDevicesUsed, GetFramesTestFixture, ::testing::Values(1, 8, 16129));
 
-TEST_P(GetSerialDataTestFixture, getSerialData_WorkspacePassed_SerialDataVectorHasTwiceAsManyElementsAsDevicesUsed) {
+TEST_P(GetFramesTestFixture, getFrames_WorkspacePassed_SerialDataVectorHasTheSameNumberOfElementsAsDevicesUsed) {
     const int devicesUsed = GetParam();
     auto stub = std::make_unique<NiceMock<WorkspaceStub>>();
     ON_CALL(*stub, devicesUsed).WillByDefault(Return(devicesUsed));
 
     const FrameBuilder sut(std::move(stub));
 
-    const auto expected = 2 * devicesUsed;
-    ASSERT_THAT(sut.getSerialData(), SizeIs(expected));
+    ASSERT_THAT(sut.getFrames(), SizeIs(devicesUsed));
 }
 
-TEST(FrameBuilderTestSuite, getSerialData_ByDefault_SerialDataVectorIsFilledWithZeros) {
+TEST(FrameBuilderTestSuite, getFrames_ByDefault_SerialDataVectorIsFilledWithZeros) {
     auto stub = std::make_unique<NiceMock<WorkspaceStub>>();
     ON_CALL(*stub, devicesUsed).WillByDefault(Return(10));
 
     const FrameBuilder sut(std::move(stub));
-    ASSERT_THAT(sut.getSerialData(), Each(0));
+    const std::pair<Register, uint8_t> expectedFrame(Register::NO_OP, 0x00);
+    ASSERT_THAT(sut.getFrames(), Each(expectedFrame));
 }
 
 TEST(FrameBuilderTestSuite, fillAll_FramePassed_AllElementsInSerialDataEqualPassedFrame) {
@@ -41,7 +41,7 @@ TEST(FrameBuilderTestSuite, fillAll_FramePassed_AllElementsInSerialDataEqualPass
 
     FrameBuilder sut(std::move(stub));
 
-    std::pair<Register, uint8_t> framePassed(Register::DISPLAY_TEST, 0xFF);
+    const std::pair<Register, uint8_t> framePassed(Register::DISPLAY_TEST, 0xFF);
     sut.fillAll(framePassed);
     const auto &actual = sut.getFrames();
 
